e949bd37e4677542501abedd5302cf53ff4776eb
LedicaCo <webdesign@ledica.co>
2025-04-16 20:38:02 -0500

    Last commit,

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import { Link, Redirect } from "expo-router";
+import { View, Text, SafeAreaView } from "react-native";
+
+const App = () => {
+  return <Redirect href="/(stack)/home" />;
+  /*  return (
+    <SafeAreaView>
+      <View className="mt-5 mx-2.5">
+        <Text className="text-5xl" style={{ fontFamily: "WorkSans-Black" }}>
+          Hola Mundo
+        </Text>
+        <Text className="text-4xl font-work-black text-primary">
+          Hola Mundo
+        </Text>
+        <Text className="text-3xl font-work-medium text-secondary">
+          Hola Mundo
+        </Text>
+        <Text className="text-2xl font-work-light text-secondary-100">
+          Hola Mundo
+        </Text>
+        <Text className="text-x text-tertiary">Hola Mundo</Text>
+
+        <Link href="/products">Productos</Link>
+      </View>
+    </SafeAreaView>
+  ); */
+};
+
+export default App;
